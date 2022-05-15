bf438b48d7dd30db8661818891b4a5952796be9e
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-05-15 10:32:11 +0000

    [Add]Helloクラスを追加

---
+++ Hello.java
@@ -0,0 +1,5 @@
+class Hello {
+  public String hello (String name) {
+    return "私の名前は " + name + " です";
+  }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
   public static void main (String[] args) {
     System.out.println("hello java!");
+    Hello hello = new Hello();
+    System.out.println(hello.hello("shino"));
   }
 }
