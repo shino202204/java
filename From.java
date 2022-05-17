a84e33c241832632e4d7ce9f192588a4c3a13f5c
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-05-17 11:32:31 +0000

    [Add]Fromクラスを追加

---
+++ From.java
@@ -0,0 +1,5 @@
+class From {
+  public String from(String from){
+    return "私の出身は" + from + "です";
+  }
+}
--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ class Main {
   public static void main (String[] args) {
     System.out.println("hello java!");
     Hello hello = new Hello();
-    System.out.println(hello.hello("shino"));
+    System.out.println(hello.hello("taro"));
     Age age = new Age();
-    System.out.println(age.age(28));
+    System.out.println(age.age(27));
+    From from = new From();
+    System.out.println(from.from("日本"));
   }
 }
