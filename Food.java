d5154395bd4b239d0dd6c41b78b79924be78e80b
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-05-22 01:22:48 +0000

    [Update]Foodクラスを更新

--- Food.java
+++ Food.java
@@ -1,8 +1,9 @@
 class Food {
-  public String food(String food, String fruit) {
+  public String food(String food, String fruit, String cake) {
     String message;
     message =  "好きな食べ物は" + food + "です";
     message += "/好きな果物は" + fruit + "です";
+    message += "/好きなケーキは" + fruit + "ケーキです";
     return message;
   }
 }
