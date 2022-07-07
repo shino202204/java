4b00bd9297eac777d9963572d10add9443242416
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-07-07 03:12:02 +0000

    [Update]Fromクラスを修正

--- From.java
+++ From.java
@@ -1,9 +1,10 @@
 class From {
-  public String from(String from, String birth, String university, String work){
+  public String from(String from, String birth, String university, String work, String future){
     String message = "生まれは" + birth + "です";
     message += "育ちは" + from + "です";
     message += "大学は時代は" + university + "で過ごしました";
     message += "職場は" + work + "です";
+    message += "将来は" + future + "に住みたいです";
     return message;
   }
 }
