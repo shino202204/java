2b2c2ef65dec9bcee3249d9586bc5dc20f4ee69c
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-07-12 12:31:37 +0000

    [Add]Fromクラスに老後の記述を追加

--- From.java
+++ From.java
@@ -1,10 +1,11 @@
 class From {
-  public String from(String from, String birth, String university, String work, String future){
+  public String from(String from, String birth, String university, String work, String future, String old){
     String message = "生まれは" + birth + "です";
     message += "育ちは" + from + "です";
     message += "大学は時代は" + university + "で過ごしました";
     message += "職場は" + work + "です";
     message += "将来は" + future + "に住みたいです";
+    message += "老後は" + old + "に住みたいです";
     return message;
   }
 }
--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ class Main {
     Age age = new Age();
     System.out.println(age.age(27));
     From from = new From();
-    System.out.println(from.from("日本", "京都", "九州", "東京", "海外"));
+    System.out.println(from.from("日本", "京都", "九州", "東京", "海外", "地元"));
     Food food = new Food();
     System.out.println(food.food("お魚","ぶどう", "チーズ", "お茶", "ブリーチーズ", "プライドポテト", "チヂミ"));
   }
