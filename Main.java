07184871744f4ecb50651d3edb25131150a37a49
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-07-14 03:02:58 +0000

    [Add]ペルー料理を追加

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ class Main {
     From from = new From();
     System.out.println(from.from("日本", "京都", "九州", "東京", "海外", "地元"));
     Food food = new Food();
-    System.out.println(food.food("お魚","ぶどう", "チーズ", "お茶", "ブリーチーズ", "プライドポテト", "チヂミ"));
+    System.out.println(food.food("お魚","ぶどう", "チーズ", "お茶", "ブリーチーズ",
+      "プライドポテト", "チヂミ", "アヒデガジーナ"));
   }
 }
