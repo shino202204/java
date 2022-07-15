f2b9852db1d85be33fbb17b501be8ca35aa15384
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-07-15 03:03:06 +0000

    [Add]Foodクラスに日本料理を追加

--- Food.java
+++ Food.java
@@ -1,6 +1,6 @@
 class Food {
   public String food(String food, String fruit, String cake, String drink, String cheese,
-    String munchie, String k_food, String p_food) {
+    String munchie, String k_food, String p_food, String j_food) {
     String message;
     message =  "好きな食べ物は" + food + "です";
     message += "/好きな果物は" + fruit + "です";
@@ -10,6 +10,7 @@ class Food {
     message += "/好きなスナック菓子は" + munchie + "です";
     message += "/好きな韓国料理は" + k_food + "です";
     message += "/好きなペルー料理は" + p_food + "です";
+    message += "/好きな日本料理は" + j_food + "です";
     return message;
   }
 }
