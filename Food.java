6f6063dd3af934b4db703c4777cb45cbd5510a82
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-07-29 02:57:38 +0000

    [Add]Foodクラスに中華料理11を追加

--- Food.java
+++ Food.java
@@ -2,7 +2,7 @@ class Food {
   public String food(String food, String fruit, String cake, String drink, String cheese,
     String munchie, String k_food, String k_food2, String p_food, String j_food, String c_food, String c_food2,
     String c_food3, String c_food4, String c_food5, String c_food6, String c_food7, String c_food8,
-    String c_food9, String c_food10) {
+    String c_food9, String c_food10, String c_food11) {
     String message;
     message =  "好きな食べ物は" + food + "です";
     message += "/好きな果物は" + fruit + "です";
@@ -24,6 +24,7 @@ class Food {
     message += "/好きな中華料理8は" + c_food8 + "です";
     message += "/好きな中華料理9は" + c_food9 + "です";
     message += "/好きな中華料理10は" + c_food10 + "です";
+    message += "/好きな中華料理11は" + c_food11 + "です";
     return message;
   }
 }
