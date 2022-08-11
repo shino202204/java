ed54f56ab6948e2876119379550206c54ceae97a
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-08-11 09:20:35 +0000

    [Add]Foodクラスに韓国料理7を追加

--- Food.java
+++ Food.java
@@ -1,7 +1,7 @@
 class Food {
   public String food(String food, String fruit, String cake, String drink, String cheese,
     String munchie, String k_food, String k_food2, String k_food3, String k_food4, String k_food5,
-    String k_food6, String p_food, String j_food,
+    String k_food6, String k_food7, String p_food, String j_food,
     String c_food, String c_food2,
     String c_food3, String c_food4, String c_food5, String c_food6, String c_food7, String c_food8,
     String c_food9, String c_food10, String c_food11, String c_food12, String c_food13, String c_food14,
@@ -19,6 +19,7 @@ class Food {
     message += "/好きな韓国料理4は" + k_food4 + "です";
     message += "/好きな韓国料理5は" + k_food5 + "です";
     message += "/好きな韓国料理6は" + k_food6 + "です";
+    message += "/好きな韓国料理7は" + k_food7 + "です";
     message += "/好きなペルー料理は" + p_food + "です";
     message += "/好きな日本料理は" + j_food + "です";
     message += "/好きな中華料理は" + c_food + "です";
